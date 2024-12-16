93c40fdff958b08cdcb892901adbb5ab1d4da88c
João Arthur Souza Santos <joaoarthursouzasantos@gmail.com>
2024-12-16 14:56:27 -0300

    cria a model Funcionario e sua pasta

---
+++ app/funcionario/model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String ,ForeignKey
+from sqlalchemy.orm import relationship
+from db.base import Base
+
+# Modelo Funcionario
+class FuncionarioModel(Base):
+    __tablename__ = "funcionarios"
+
+    cpf = Column(String(255), primary_key=True, index=True)
+    password = Column(String(255), nullable=False)
+    nome = Column(String(255), unique=True, index=True, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    idPerfil = Column(int, ForeignKey("perfis.id"), nullable=False, index=True)
+
+    perfil = relationship("PerfilModel", back_populates="funcionarios")
+
