9f6938ae329a1c09645a8ad92140fb21e5770310
Murilo Santos Ruas <murilosruas1@gmail.com>
2024-12-16 15:08:54 -0300

    Atualiza pasta paciente

---
+++ app/paciente/model.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String , Date
+from sqlalchemy.orm import relationship
+from shared.database import Base
+
+class Paciente(Base):
+    __tablename__ = "pacientes"
+
+    nome = Column(String(255))
+    email = Column(String(255))
+    cpf = Column(String(255), index=True)
+    telefone = Column(String(255))
+    microRegiao = Column(String(255))
+    numeroSUS = Column(String(255), primary_key=True, index=True)  # Número SUS como chave primária
+    dataNascimento = Column(Date)
+    sexo = Column(String(255), index=True)
+    info = Column(String(255), index=True)
+    
+    estratificacao = relationship("Estratificacao", back_populates="paciente")
+    retornos = relationship("Retornos", back_populates="paciente")
+    
+    #pessoa = relationship("Pessoa", back_populates="paciente")
+    #consultas = relationship("Consulta", back_populates="paciente")
