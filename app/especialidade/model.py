e3b9f3f099a78b2c8252a97732d9a7d1dcb51314
João Arthur Souza Santos <joaoarthursouzasantos@gmail.com>
2024-12-17 14:43:26 -0300

    cria model especialidade

---
+++ app/especialidade/model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from db.base import Base
+
+class EspecialidadeModel(Base):
+    __tablename__ = "especialidades"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(255), unique=True, nullable=False)
+
+    # Relacionamento com MedicoModel
+    medicos = relationship("MedicoModel", back_populates="especialidade")
--- app/medico/model.py
+++ app/medico/model.py
@@ -8,11 +8,16 @@ class MedicoModel(Base):
 
     crm = Column(String(20), primary_key=True, index=True)
     nome = Column(String(255), nullable=False)
-    especialidade = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, index=True, nullable=False)
     telefone = Column(String(20), nullable=True)
+    idPerfil = Column(Integer, ForeignKey("perfis.id"), nullable=False, index=True)
+
+    # Chave estrangeira para Especialidade
+    especialidade_id = Column(Integer, ForeignKey("especialidades.id"), nullable=False)
 
     # Relacionamentos
+    perfil = relationship("PerfilModel", back_populates="medico")
     consultas = relationship("ConsultaModel", back_populates="medico")
     prontuarios = relationship("ProntuarioModel", back_populates="medico")
     prescricoes = relationship("PrescricaoModel", back_populates="medico")
+    especialidade = relationship("EspecialidadeModel", back_populates="medicos")
