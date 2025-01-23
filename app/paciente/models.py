a7ffe92b28131caf1f591333aa4d273291adf96d
Murilo Santos Ruas <murilosruas1@gmail.com>
2025-01-23 16:49:24 -0300

    Adiciona relacionamento entre paciente e biometria

--- app/paciente/models.py
+++ app/paciente/models.py
@@ -1,15 +1,18 @@
-from sqlalchemy import Column, String , Date , ForeignKey
+from sqlalchemy import Column, VARCHAR
 from sqlalchemy.orm import relationship
 from db.base import Base
 
-class Paciente(Base):
+class PacienteModel(Base):
     __tablename__ = "pacientes"
 
-    numeroSUS = Column(String(255), primary_key=True, index=True)  # Número SUS como chave primária  # Chave estrangeira referenciando o CPF na tabela pessoas
-    dataNascimento = Column(Date)
-    sexo = Column(String(255), index=True)
-    info = Column(String(255), index=True)
-    pessoa = relationship("Pessoa", back_populates="paciente")
-    consultas = relationship("Consulta", back_populates="paciente")
+    numeroSUS = Column(VARCHAR(45), primary_key=True, index=True)  # Número SUS como chave primária
+    email = Column(VARCHAR(45))
+    telefone = Column(VARCHAR(45))
+    sexo = Column(VARCHAR(45), index=True)
+    info = Column(VARCHAR(45), index=True)
+    dataNascimento = Column(VARCHAR(45))
+    microRegiao = Column(VARCHAR(45))
 
+    consulta = relationship("consulta", back_populates="consulta")
+    biometria = relationship("biometria")
 
